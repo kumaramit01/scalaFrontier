d3ccb602ec71afa557174415a1a910b41ea6428f
Amit Kumar <kumaramit01@gmail.com>
2011-04-29 15:48:43 -0600

    added inventory and location

---
+++ src/org/scala/training/model/Inventory.scala
@@ -0,0 +1,11 @@
+package org.scala.training.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: amitku
+ * Date: 4/29/11
+ * Time: 3:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Inventory
